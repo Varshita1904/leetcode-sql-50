d48cbfa61ebe64a8e7dd5f596fe5249dee31d7d5
Varshita1904 <155021724+Varshita1904@users.noreply.github.com>
2026-01-23 18:36:42 -0600

    Create 012_students_and_examinations.sql

---
+++ 012_students_and_examinations.sql
@@ -0,0 +1,12 @@
+with cte as(
+    select student_id, student_name, subject_name
+from Students join Subjects)
+
+select s.student_id, s.student_name, s.subject_name,
+count(e.subject_name) 
+as attended_exams
+from cte s left join Examinations e
+on s.student_id=e.student_id and s.subject_name=e.subject_name
+group by s.student_id, s.student_name, s.subject_name
+order by s.student_id, s.subject_name
+ 
